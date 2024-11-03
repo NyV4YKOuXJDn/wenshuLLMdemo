ea494e18150983c671fe038b18090812290dbc9a
NyV4YKOuXJDn <96531658+NyV4YKOuXJDn@users.noreply.github.com>
2024-11-03 19:20:18 +0800

    11031920

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import './App.css';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
