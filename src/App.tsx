623b905fa2ae71aa0a933f61ddc349744ac3c18e
NyV4YKOuXJDn <96531658+NyV4YKOuXJDn@users.noreply.github.com>
2024-11-03 19:39:55 +0800

    11031939

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import ScenePreview from './scene-preview';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ScenePreview />} />
       </Routes>
     </Router>
   );
